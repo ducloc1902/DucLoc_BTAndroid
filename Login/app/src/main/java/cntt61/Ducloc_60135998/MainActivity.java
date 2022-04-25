794698c5737dc3c436cd00a66c1620f4602d0408
ducloc1902 <62890971+ducloc1902@users.noreply.github.com>
2022-04-25 22:19:01 +0700

    sua_loi

--- Login/app/src/main/java/cntt61/Ducloc_60135998/MainActivity.java
+++ Login/app/src/main/java/cntt61/Ducloc_60135998/MainActivity.java
@@ -3,6 +3,9 @@ package cntt61.Ducloc_60135998;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnLogin);
+        btn.setOnClickListener(ChuyensangLogin);
     }
+    View.OnClickListener ChuyensangLogin = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent iLogin = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(iLogin);
+        }
+    };
 }
